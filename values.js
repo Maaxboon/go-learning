aaef5a68dadae9390a8d4c7c54b6f8c32f436cab
Maaxboon <maaxwelokoth@gmail.com>
2025-01-03 20:17:37 +0300

    Adds JavaScript data types/values

---
+++ values.js
@@ -0,0 +1,24 @@
+// Primitive data types
+// Numbers
+42;
+3.14;
+
+// Strings
+"Hello, friend.";
+
+// Booleans
+true;
+false;
+
+// empty values
+null;
+undefined
+
+// Non primitive values
+// Arrays
+[1, 2, 3)];
+
+// Objects
+{
+  name: Maaxboon;
+}

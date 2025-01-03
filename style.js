8c7362a2fbf3e42c83f8d8ff51da9ca47a4f9a4c
Maaxboon <maaxwelokoth@gmail.com>
2025-01-03 20:17:13 +0300

    Adds JavaScript styling

---
+++ style.js
@@ -0,0 +1,22 @@
+// Single line comments
+if (isNegativeNumber(value)) {
+  return false; // We don't know how to handle negative numbers
+}
+
+/*Multi
+line
+comments */
+
+// Functions, methods or variable names should be camelcase
+// Assigning a variable
+let fourAndTwentyBLackBirds = 24;
+
+// Defining a function
+function thisIsAGreatMethod() {
+  // Do some stuff
+}
+
+// filenames typically use snake_case or kebab-case: all lowercase letters with individual words separated with underscores(_) or hyphens(-)
+
+hello_world.js; // snake_case
+hello - world.js; // kebab-case

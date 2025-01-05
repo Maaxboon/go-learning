0cc9a1b42cc432a52bf5d9e5497ef08a04d11c1e
Maaxboon <maaxwelokoth@gmail.com>
2025-01-05 18:11:19 +0300

    Adds js apis

---
+++ js-apis/src/index.js
@@ -0,0 +1,7 @@
+// The API getting:https://catfact.ninja/fact 
+const bttn = document.querySelector{*#random-bttn*};
+bttn.addEventListener{*click*, handleClick};
+
+function handleClick() {
+    // write your code here
+}
